f3d3a872e893fd1fc6bc2242b9c81c40f08e7d5b
Joscha von Andrian <joschavonandrian@gmail.com>
2021-12-31 18:39:24 +0100

    add youtube webview

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:get/get.dart';
+
+import 'home_controller.dart';
+
+class HomePage extends GetView<HomeController> {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Get.put(HomeController());
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 0,
+      ),
+      body: InAppWebView(
+        key: controller.webViewKey,
+        onWebViewCreated: (webController) {
+          webController.loadUrl(
+              urlRequest: URLRequest(
+            url: MediaQuery.of(context).platformBrightness == Brightness.dark
+                ? Uri.parse(controller.url + "?theme=dark")
+                : Uri.parse(controller.url),
+          ));
+        },
+      ),
+    );
+  }
+}
